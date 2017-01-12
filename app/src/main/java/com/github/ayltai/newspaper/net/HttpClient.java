c331b39f2c04f1a0f77ad1d1fad441c55bd4591f
Alan Tai <alan@9gag.com>
2017-01-12 13:05:22 +0800

    Refactor HTTP client

---
+++ app/src/main/java/com/github/ayltai/newspaper/net/HttpClient.java
@@ -0,0 +1,76 @@
+package com.github.ayltai.newspaper.net;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.github.ayltai.newspaper.rss.Client;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public final class HttpClient implements Closeable {
+    //region Constants
+
+    private static final long CONNECT_TIMEOUT = 60;
+    private static final long READ_TIMEOUT    = 60;
+    private static final long WRITE_TIMEOUT   = 60;
+
+    //endregion
+
+    private final List<Call> calls = new ArrayList<>();
+
+    private final OkHttpClient client = new OkHttpClient.Builder()
+        .connectTimeout(HttpClient.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(HttpClient.READ_TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(HttpClient.WRITE_TIMEOUT, TimeUnit.SECONDS)
+        .build();
+
+    public HttpClient() {
+    }
+
+    @Override
+    public void close() {
+        synchronized (this.calls) {
+            for (final Call call : this.calls) {
+                if (!call.isCanceled()) call.cancel();
+            }
+
+            this.calls.clear();
+        }
+    }
+
+    public static void closeQuietly(@Nullable final InputStream inputStream) {
+        try {
+            if (inputStream != null) inputStream.close();
+        } catch (final IOException e) {
+            Log.e(Client.class.getName(), e.getMessage(), e);
+        }
+    }
+
+    public InputStream download(@NonNull final String url) throws IOException {
+        final Call call = this.client.newCall(new Request.Builder().url(url).build());
+        this.calls.add(call);
+
+        final Response response = call.execute();
+
+        try {
+            if (response.isSuccessful()) {
+                return response.body().byteStream();
+            } else {
+                throw new IOException("Unexpected response " + response);
+            }
+        } finally {
+            this.calls.remove(call);
+        }
+    }
+}
