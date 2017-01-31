895b5c1700987318bae88dd15b639f7d72632bc6
Alan Tai <ayltai@gmail.com>
2017-01-31 14:15:11 +0800

    Fix CircleCI build script

--- scripts/ci-helpers.sh
+++ scripts/ci-helpers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function generateTestReports() {
-    ./gradlew -i testDebugUnitTest jacocoReport uploadCoverageToCodacy --console=plain
+    ./gradlew -i testDebugUnitTest jacocoTestReport uploadCoverageToCodacy --console=plain
     cp -r app/build/test-results/testDebugUnitTest/debug/*.xml $CIRCLE_TEST_REPORTS && cp -r app/build/reports/tests/testDebugUnitTest $CIRCLE_TEST_REPORTS && cp -r app/build/reports/jacoco/jacocoTestReport $CIRCLE_TEST_REPORTS
 }
 
