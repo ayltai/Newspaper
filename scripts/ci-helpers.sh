78ba6c084fb575b8529eb9d856e95293128e067a
Alan Tai <ayltai@gmail.com>
2017-01-31 13:25:52 +0800

    Fix Jacoco not working with Robolectric

--- scripts/ci-helpers.sh
+++ scripts/ci-helpers.sh
@@ -2,7 +2,7 @@
 
 function generateTestReports() {
     ./gradlew -i testDebugUnitTest jacocoReport uploadCoverageToCodacy --console=plain
-    cp -r app/build/test-results/testDebugUnitTest/debug/*.xml $CIRCLE_TEST_REPORTS && cp -r app/build/reports/tests/testDebugUnitTest $CIRCLE_TEST_REPORTS && cp -r app/build/reports/jacoco/jacocoReport $CIRCLE_TEST_REPORTS
+    cp -r app/build/test-results/testDebugUnitTest/debug/*.xml $CIRCLE_TEST_REPORTS && cp -r app/build/reports/tests/testDebugUnitTest $CIRCLE_TEST_REPORTS && cp -r app/build/reports/jacoco/jacocoTestReport $CIRCLE_TEST_REPORTS
 }
 
 function inspectCode() {
