2d733fb2ec45d5d3d2615dee95d7784cc8839ee8
Alan Tai <ayltai@gmail.com>
2017-05-07 20:03:25 +0800

    #45 [WIP] Parse full description and images from HKET news

--- app/src/main/java/com/github/ayltai/newspaper/client/HketClient.java
+++ app/src/main/java/com/github/ayltai/newspaper/client/HketClient.java
@@ -1,18 +1,27 @@
 package com.github.ayltai.newspaper.client;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import org.apache.commons.io.IOUtils;
+
 import com.github.ayltai.newspaper.client.rss.RssClient;
+import com.github.ayltai.newspaper.model.Image;
 import com.github.ayltai.newspaper.model.Item;
 import com.github.ayltai.newspaper.model.Source;
 import com.github.ayltai.newspaper.net.HttpClient;
+import com.github.ayltai.newspaper.util.StringUtils;
 
+import rx.Emitter;
 import rx.Observable;
 
 final class HketClient extends RssClient {
+    private static final String TAG_QUOTE = "\"";
+
     @Inject
     HketClient(@NonNull final HttpClient client, @Nullable final Source source) {
         super(client, source);
@@ -21,6 +30,31 @@ final class HketClient extends RssClient {
     @NonNull
     @Override
     public Observable<Item> updateItem(@NonNull final Item item) {
-        return null;
+        return Observable.create(emitter -> {
+            try {
+                final String html = StringUtils.substringBetween(IOUtils.toString(this.client.download(item.getLink()), Client.ENCODING), "<div class=\"article-detail\">", "<div class=\"article-detail_facebook-like\">");
+
+                final String[] imageContainers = StringUtils.substringsBetween(html, "<img ", "/>");
+
+                for (final String imageContainer : imageContainers) {
+                    final String imageUrl         = StringUtils.substringBetween(imageContainer, "data-src=\"",HketClient. TAG_QUOTE);
+                    final String imageDescription = StringUtils.substringBetween(imageContainer, "alt=\"", HketClient.TAG_QUOTE);
+
+                    if (imageUrl != null) item.getImages().add(new Image(imageUrl, imageDescription));
+                }
+
+                final String[]      contents = StringUtils.substringsBetween(html, "<p>", "</p>");
+                final StringBuilder builder  = new StringBuilder();
+
+                for (final String content : contents) builder.append(content).append("<br>");
+
+                item.setDescription(builder.toString());
+                item.setIsFullDescription(true);
+
+                emitter.onNext(item);
+            } catch (final IOException e) {
+                emitter.onError(e);
+            }
+        }, Emitter.BackpressureMode.BUFFER);
     }
 }
