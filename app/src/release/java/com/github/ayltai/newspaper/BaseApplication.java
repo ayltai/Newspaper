132e7ff719d5d927fe1b82f10b55347c021a0625
Alan Tai <ayltai@gmail.com>
2017-06-15 11:29:44 +0800

    Remove multi-dex support for release build

--- app/src/release/java/com/github/ayltai/newspaper/BaseApplication.java
+++ app/src/release/java/com/github/ayltai/newspaper/BaseApplication.java
@@ -1,13 +1,13 @@
 package com.github.ayltai.newspaper;
 
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
 
 import io.fabric.sdk.android.Fabric;
 
-public abstract class BaseApplication extends MultiDexApplication {
+public abstract class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
