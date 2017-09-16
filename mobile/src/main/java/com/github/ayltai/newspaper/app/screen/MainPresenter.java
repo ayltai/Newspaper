1d8c4543eba2d9d63e201f820fee9b081e33a9b2
Alan Tai <ayltai@gmail.com>
2017-09-16 18:05:18 +0800

    #79 Add expandable floating action buttons

--- mobile/src/main/java/com/github/ayltai/newspaper/app/screen/MainPresenter.java
+++ mobile/src/main/java/com/github/ayltai/newspaper/app/screen/MainPresenter.java
@@ -2,6 +2,7 @@ package com.github.ayltai.newspaper.app.screen;
 
 import android.support.annotation.NonNull;
 
+import com.github.ayltai.newspaper.util.Irrelevant;
 import com.github.ayltai.newspaper.view.ScreenPresenter;
 
 import io.reactivex.Flowable;
@@ -9,6 +10,15 @@ import io.reactivex.Flowable;
 public class MainPresenter extends ScreenPresenter<MainPresenter.View> {
     public interface View extends ScreenPresenter.View {
         @NonNull
-        Flowable<Integer> pageChanges();
+        Flowable<Irrelevant> goTopActions();
+
+        @NonNull
+        Flowable<Irrelevant> refreshActions();
+
+        @NonNull
+        Flowable<Irrelevant> filterActions();
+
+        @NonNull
+        Flowable<Integer> pageSelections();
     }
 }
--- mobile/src/main/java/com/github/ayltai/newspaper/app/screen/MainScreen.java
+++ mobile/src/main/java/com/github/ayltai/newspaper/app/screen/MainScreen.java
@@ -9,17 +9,22 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.annotation.StyleRes;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 
 import com.google.auto.value.AutoValue;
 
 import com.github.ayltai.newspaper.R;
+import com.github.ayltai.newspaper.util.Irrelevant;
 import com.github.ayltai.newspaper.widget.Screen;
+import com.jakewharton.rxbinding2.support.v4.view.RxViewPager;
+import com.jakewharton.rxbinding2.view.RxView;
 
 import flow.ClassKey;
 import io.reactivex.Flowable;
@@ -36,17 +41,29 @@ public final class MainScreen extends Screen implements MainPresenter.View {
 
     public static final MainScreen.Key KEY = MainScreen.Key.create();
 
-    private final FlowableProcessor<Integer> pageChanges = PublishProcessor.create();
+    //region Subscriptions
+
+    private final FlowableProcessor<Irrelevant> goTopActions   = PublishProcessor.create();
+    private final FlowableProcessor<Irrelevant> refreshActions = PublishProcessor.create();
+    private final FlowableProcessor<Irrelevant> filterActions  = PublishProcessor.create();
+    private final FlowableProcessor<Integer>    pageSelections = PublishProcessor.create();
+
+    //endregion
 
     //region Components
 
-    private Toolbar   toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private Toolbar              toolbar;
+    private FloatingActionButton goTopAction;
+    private FloatingActionButton refreshAction;
+    private FloatingActionButton filterAction;
+    private FloatingActionButton moreAction;
+    private TabLayout            tabLayout;
+    private ViewPager            viewPager;
 
     //endregion
 
     private MainAdapter adapter;
+    private boolean     isMoreActionsShown;
 
     //region Constructors
 
@@ -73,22 +90,72 @@ public final class MainScreen extends Screen implements MainPresenter.View {
 
     //endregion
 
+    //region Events
+
     @NonNull
     @Override
-    public Flowable<Integer> pageChanges() {
-        return this.pageChanges;
+    public Flowable<Irrelevant> goTopActions() {
+        return this.goTopActions;
     }
+
+    @NonNull
+    @Override
+    public Flowable<Irrelevant> refreshActions() {
+        return this.refreshActions;
+    }
+
+    @NonNull
+    @Override
+    public Flowable<Irrelevant> filterActions() {
+        return this.filterActions;
+    }
+
+    @NonNull
+    @Override
+    public Flowable<Integer> pageSelections() {
+        return this.pageSelections;
+    }
+
+    //endregion
 
     @Override
     protected void onAttachedToWindow() {
         if (this.isFirstTimeAttachment) {
+            this.manageDisposable(RxView.clicks(this.moreAction).subscribe(irrelevant -> {
+                if (this.isMoreActionsShown) {
+                    this.hideMoreActions();
+                } else {
+                    this.showMoreActions();
+                }
+            }));
+
+            this.manageDisposable(RxView.clicks(this.goTopAction).subscribe(irrelevant -> {
+                this.hideMoreActions();
+
+                this.goTopActions.onNext(Irrelevant.INSTANCE);
+            }));
+
+            this.manageDisposable(RxView.clicks(this.refreshAction).subscribe(irrelevant -> {
+                this.hideMoreActions();
+
+                this.refreshActions.onNext(Irrelevant.INSTANCE);
+            }));
+
+            this.manageDisposable(RxView.clicks(this.filterAction).subscribe(irrelevant -> {
+                this.hideMoreActions();
+
+                this.filterActions.onNext(Irrelevant.INSTANCE);
+            }));
+
             this.adapter = new MainAdapter(this.getContext());
 
             final LifecycleOwner lifecycleOwner = this.getLifecycleOwner();
             if (lifecycleOwner != null) lifecycleOwner.getLifecycle().addObserver(this.adapter);
 
             this.viewPager.setAdapter(this.adapter);
-            this.pageChanges.onNext(0);
+            this.manageDisposable(RxViewPager.pageSelections(this.viewPager).subscribe(this.pageSelections::onNext));
+
+            this.pageSelections.onNext(0);
         }
 
         super.onAttachedToWindow();
@@ -97,12 +164,34 @@ public final class MainScreen extends Screen implements MainPresenter.View {
     private void init() {
         final View view = LayoutInflater.from(this.getContext()).inflate(R.layout.screen_main, this, true);
 
-        this.toolbar   = view.findViewById(R.id.toolbar);
-        this.tabLayout = view.findViewById(R.id.tabLayout);
-        this.viewPager = view.findViewById(R.id.viewPager);
+        this.toolbar       = view.findViewById(R.id.toolbar);
+        this.goTopAction   = view.findViewById(R.id.action_go_top);
+        this.refreshAction = view.findViewById(R.id.action_refresh);
+        this.filterAction  = view.findViewById(R.id.action_filter);
+        this.moreAction    = view.findViewById(R.id.action_more);
+        this.tabLayout     = view.findViewById(R.id.tabLayout);
+        this.viewPager     = view.findViewById(R.id.viewPager);
 
         this.toolbar.inflateMenu(R.menu.main);
 
         this.tabLayout.setupWithViewPager(this.viewPager, true);
+    }
+
+    private void showMoreActions() {
+        this.isMoreActionsShown = true;
+
+        this.moreAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.rotate_clockwise));
+        this.goTopAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_open));
+        this.refreshAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_open));
+        this.filterAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_open));
+    }
+
+    private void hideMoreActions() {
+        this.isMoreActionsShown = false;
+
+        this.moreAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.rotate_anti_clockwise));
+        this.goTopAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_close));
+        this.refreshAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_close));
+        this.filterAction.startAnimation(AnimationUtils.loadAnimation(this.getContext(), R.anim.fab_close));
     }
 }
