90fc9eb5f245afc985421d694f82fdfb1d638570
Alan Tai <ayltai@gmail.com>
2017-10-13 22:07:12 +0800

    #85 Add unit tests for debug policy settings

---
+++ mobile/src/test/java/com/github/ayltai/newspaper/debug/VmPolicyFactoryTest.java
@@ -0,0 +1,14 @@
+package com.github.ayltai.newspaper.debug;
+
+import org.junit.Test;
+
+import com.github.ayltai.newspaper.UnitTest;
+
+import junit.framework.Assert;
+
+public final class VmPolicyFactoryTest extends UnitTest {
+    @Test
+    public void test() {
+        Assert.assertEquals("[StrictMode.VmPolicy; mask=-2147109632]", VmPolicyFactory.newVmPolicy().toString());
+    }
+}
