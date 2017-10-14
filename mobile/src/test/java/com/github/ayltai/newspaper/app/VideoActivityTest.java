ee542ed526cb7221a116c999a443631afc98a73d
Alan Tai <ayltai@gmail.com>
2017-10-14 16:39:43 +0800

    #85 Add instrumented tests for VideoActivity

---
+++ mobile/src/test/java/com/github/ayltai/newspaper/app/VideoActivityTest.java
@@ -0,0 +1,20 @@
+package com.github.ayltai.newspaper.app;
+
+import android.content.Intent;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.robolectric.RuntimeEnvironment;
+
+import com.github.ayltai.newspaper.UnitTest;
+
+public final class VideoActivityTest extends UnitTest {
+    @Test
+    public void testCreateIntent() {
+        final Intent intent = VideoActivity.createIntent(RuntimeEnvironment.application, "url", false, 0);
+
+        Assert.assertEquals("url", intent.getStringExtra(VideoActivity.EXTRA_VIDEO_URL));
+        Assert.assertEquals(false, intent.getBooleanExtra(VideoActivity.EXTRA_IS_PLAYING, false));
+        Assert.assertEquals(0, intent.getLongExtra(VideoActivity.EXTRA_SEEK_POSITION, 0));
+    }
+}
