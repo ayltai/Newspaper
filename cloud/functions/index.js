67cb7cba4667fd71c0190b941480a6de74bd652f
Alan Tai <ayltai@gmail.com>
2017-10-19 18:44:21 +0800

    #107 Fix Google API token retrieval Firebase Cloud Functions

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -2,27 +2,22 @@
 
 const functions = require('firebase-functions');
 const google    = require('googleapis');
-
-const CONFIG_CLIENT_ID     = functions.config().googleapi.client_id;
-const CONFIG_CLIENT_SECRET = functions.config().googleapi.client_secret;
-
-const SCOPES             = ['https://www.googleapis.com/auth/cloud-language'];
-const FUNCTIONS_REDIRECT = `${process.env.GCLOUD_PROJECT}.firebaseapp.com/oauthCallback`;
-
-const client = new google.auth.OAuth2(CONFIG_CLIENT_ID, CONFIG_CLIENT_SECRET, FUNCTIONS_REDIRECT);
+const client    = new google.auth.OAuth2(functions.config().googleapi.client_id, functions.config().googleapi.client_secret, `https://us-central1-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/oauthCallback`);
 
 exports.getToken = functions.https.onRequest((request, response) => {
     response.redirect(client.generateAuthUrl({
         access_type : 'offline',
-        scope       : SCOPES,
+        scope       : [ 'https://www.googleapis.com/auth/cloud-language' ],
         prompt      : 'consent'
     }));
 });
 
 exports.oauthCallback = functions.https.onRequest((request, response) => {
+    console.log('Request query: ' + request.query);
+
     client.getToken(request.query.code, (error, tokens) => {
         if (error) {
-            response.status(400).send(error);
+            response.status(403).send(error);
         } else {
             response.status(200).send(tokens);
         }
