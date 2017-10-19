76724382021a703537f98d35bf6d2ac2e019c15e
Alan Tai <ayltai@gmail.com>
2017-10-19 23:54:11 +0800

    Update dependencies

--- mobile/src/main/java/com/github/ayltai/newspaper/analytics/AnalyticsComponent.java
+++ mobile/src/main/java/com/github/ayltai/newspaper/analytics/AnalyticsComponent.java
@@ -2,10 +2,13 @@ package com.github.ayltai.newspaper.analytics;
 
 import javax.inject.Singleton;
 
+import android.support.annotation.NonNull;
+
 import dagger.Component;
 
 @Singleton
 @Component(modules = { AnalyticsModule.class })
 public interface AnalyticsComponent {
+    @NonNull
     EventLogger eventLogger();
 }
